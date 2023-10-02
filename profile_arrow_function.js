faf7a06daa18d7cb07822348892934e94251e8a2
Piaripiani01 <141712563+Piaripiani01@users.noreply.github.com>
2023-10-02 14:03:38 +0700

    Add files via upload

---
+++ profile_arrow_function.js
@@ -0,0 +1,5 @@
+const login = (namadepan, namabelakang, email) => {
+  console.log("Login Screen" + namadepan + " " + namabelakang + " " + email )
+}
+
+login("pia", "ripiani", "222310271@smkn4-tsm.sch.id")
---
+++ profile_reg_function.js
@@ -0,0 +1,6 @@
+function home(namadepan, namabelakang, email) {
+  console.log("Halo " + namadepan + " " + namabelakang + " " + email )
+  console.log("Selamat Datang di Home Screen")
+}
+
+home("pia", "ripiani", "222310271@smkn4-tsm.sch.id")
